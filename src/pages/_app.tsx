451f2513c66edfc2afa9b9528e67bca30d2df4fe
Miroslavetsh <dfgshte@gmail.com>
2022-01-02 06:58:22 +0200

    Fix trpc url gen, style button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,15 +8,20 @@ function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
+const getBaseURL = () => {
+  if (process.browser) return ''
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}/api/trpc`
+
+  return `http://localhost:${process.env.PORT ?? 3000}/api/trpc`
+}
+
 export default withTRPC<AppRouter>({
   config({ ctx }) {
     /**
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc'
+    const url = `${getBaseURL()}/api/trpc`
 
     return {
       url,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,17 @@ const Home: NextPage = () => {
   if (firstPokemon.isLoading || secondPokemon.isLoading)
     return <div className='flex-col text-center align-middle'>Loading...</div>
 
+  const voteForRoundest = (selected: number) => {
+    return () => {
+      // TODO: fire mutation to persist changes
+      console.log(selected)
+      setIds(getOptionsForVote())
+    }
+  }
+
+  const buttonClass =
+    'bg-white hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mb-4'
+
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center'>
       <div className='text-2xl text-center'>Which Pokemon is Rounder?</div>
@@ -22,7 +33,7 @@ const Home: NextPage = () => {
       <div className='p-2' />
 
       <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
-        <div className='w-64 h-64 flex flex-col '>
+        <div className='w-64 h-64 flex flex-col p-4'>
           <img
             className='w-full'
             src={firstPokemon.data?.sprites.front_default || ''}
@@ -30,11 +41,15 @@ const Home: NextPage = () => {
           />
 
           <div className='text-center capitalize mt-[-2rem]'>{firstPokemon.data?.name}</div>
+
+          <button onClick={voteForRoundest(first)} className={buttonClass}>
+            Rounder
+          </button>
         </div>
 
         <div className='p-8'>vs</div>
 
-        <div className='w-64 h-64 flex flex-col '>
+        <div className='w-64 h-64 flex flex-col p-4'>
           <img
             className='w-full'
             src={secondPokemon.data?.sprites.front_default || ''}
@@ -42,9 +57,11 @@ const Home: NextPage = () => {
           />
 
           <div className='text-center capitalize mt-[-2rem]'>{secondPokemon.data?.name}</div>
-        </div>
 
-        <div className='p-2'></div>
+          <button onClick={voteForRoundest(second)} className={buttonClass}>
+            Rounder
+          </button>
+        </div>
       </div>
     </div>
   )
